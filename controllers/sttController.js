b7f3d9f39291b1e77d831d9675ef037471ddeed2
Joshua Sproule <joshsproule@live.co.uk>
2024-10-14 00:15:55 +0100

    Added Hugging Face Transcription API
    dependency prune

--- controllers/sttController.js
+++ controllers/sttController.js
@@ -36,6 +36,41 @@ const OpenAITranscription = async (req, res) => {
 
 }
 
+
+const HuggingFaceTranscription = async (req, res) => {
+
+      
+    try {
+        const params = new URLSearchParams({
+            ...url.parse(req.url, true).query //Query parameters passed to the proxy e.g city here
+        })
+
+        const data = req.body;
+        const options = {
+            headers: {
+                'Authorization': `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
+                'Content-Type': 'application/json'
+
+            }
+        };
+        //Call the actual api here using needle
+        const apiResponse = await needle('post', `${process.env.HF_WHISPER_URL}?${params}`, data, options);
+        
+        if (apiResponse.statusCode == 200) {
+            res.set('Content-Type', 'application/json')
+            res.send(apiResponse.body)
+        } else {
+            res.set('Content-Type', 'plain/text')
+            res.status(apiResponse.statusCode).send(apiResponse.body);
+        }
+    }
+    catch (err) {
+        console.log(err.message)       
+        res.status(500).json(err.message);
+    }
+}
+
 module.exports = {
-    OpenAITranscription
+    OpenAITranscription,
+    HuggingFaceTranscription
 }
--- routes/sttRoute.js
+++ routes/sttRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const sttController = require('../controllers/sttController');
 
 router.route('/openai').post(sttController.OpenAITranscription)
+router.route('/huggingface').post(sttController.HuggingFaceTranscription)
 
 module.exports = router; 
