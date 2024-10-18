c2c7f38428d3f39da38c7d2f2a8cc0b74cad52e5
Joshua Sproule <joshsproule@live.co.uk>
2024-10-18 23:41:34 +0100

    Added option to use Claude as AI

--- controllers/aiChatController.js
+++ controllers/aiChatController.js
@@ -3,29 +3,33 @@ const needle = require('needle');
 const OpenAI = require('openai');
 const fs = require('fs');
 const url = require('url');
+const Anthropic = require('@anthropic-ai/sdk');
+
+const openai = new OpenAI();
+const anthropic = new Anthropic();
 
 
 //create the route
 const OpenAIChat = async (req, res) => {
-
-
-    const openai = new OpenAI();    
-  
-    const chatCompletion = await openai.chat.completions.create(req.body)   
-    .catch(async (err) => {
-        if (err instanceof OpenAI.APIError) {
-            console.log(err.status); // 400
-            console.log(err.name); // BadRequestError
-            console.log(err.headers); // {server: 'nginx', ...}
-            console.log(err.message);
-        } else {
-            throw err;
-        }
-    });
+    const chatCompletion = await openai.chat.completions.create(req.body)
+        .catch(async (err) => {
+            if (err instanceof OpenAI.APIError) {
+                console.log(err.status); // 400
+                console.log(err.name); // BadRequestError
+                console.log(err.headers); // {server: 'nginx', ...}
+                console.log(err.message);
+                res.json(err);
+            } else {
+                throw err;
+            }
+        });
 
     if (chatCompletion) {
+
+        const response = { message: chatCompletion.choices[0].message.content }
+
         res.set('Content-Type', 'application/json')
-        res.send(chatCompletion)
+        res.json(response);
     } else {
         res.set('Content-Type', 'plain/text')
         res.status(500).send("Error");
@@ -33,6 +37,42 @@ const OpenAIChat = async (req, res) => {
 
 }
 
+const AnthropicChat = async (req, res) => {
+    const { messages, model } = req.body;
+    const system = messages.shift().content;
+
+
+    const message = await anthropic.messages.create({
+        max_tokens: 1024,
+        system: system,
+        messages: messages,
+        model: model,
+    }).catch(async (err) => {
+        if (err instanceof Anthropic.APIError) {
+            console.log(err.status); // 400
+            console.log(err.name); // BadRequestError
+            console.log(err.headers); // {server: 'nginx', ...}
+            console.log(err.message);            
+        } else {
+            throw err;
+        }
+    });
+ 
+    if (message?.type === "message") {
+        const response = { message: message.content[0].text.substring(message.content[0].text.lastIndexOf('*')+1) }
+
+        res.set('Content-Type', 'application/json')
+        res.status(200).json(response);
+    } else if (message?.type === "error") { 
+        res.json(message.error)
+    }
+    else {
+        res.set('Content-Type', 'plain/text')
+        res.status(500).send("Error");
+    }
+}
+
 module.exports = {
-    OpenAIChat
+    OpenAIChat,
+    AnthropicChat
 }
--- routes/aiChatRoute.js
+++ routes/aiChatRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const aiChatController = require('../controllers/aiChatController');
 
 router.route('/openai').post(aiChatController.OpenAIChat)
+router.route('/anthropic').post(aiChatController.AnthropicChat)
 
 module.exports = router; 
