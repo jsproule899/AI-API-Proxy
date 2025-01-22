dd6ff2403ab554b3d2531af19f90b7a7bc41db88
Joshua Sproule <joshsproule@live.co.uk>
2025-01-22 23:49:38 +0000

    Refactor: Anthropic endpoint changed to Claude

--- routes/aiChatRoute.js
+++ routes/aiChatRoute.js
@@ -4,7 +4,7 @@ const aiChatController = require('../controllers/aiChatController');
 
 router.route('/openai').post(aiChatController.OpenAIChat)
 router.route('/openai/models').get(aiChatController.OpenAIModels)
-router.route('/anthropic').post(aiChatController.AnthropicChat)
+router.route('/claude').post(aiChatController.AnthropicChat)
 router.route('/deepseek').post(aiChatController.DeepSeekChat)
 
 module.exports = router; 
